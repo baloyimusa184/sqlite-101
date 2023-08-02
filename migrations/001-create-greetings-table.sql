75c7d44eb8d8f66dc5ba91e1fff75abb93b1ae94
baloyimusa184 <baloyimusa184@gmail.com>
2023-08-02 11:16:47 +0200

    third commit of migrations folder

---
+++ migrations/001-create-greetings-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS greetings(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    language TEXT,
+    greeting TEXT
+);
---
+++ migrations/002-create-greetings-data.sql
@@ -0,0 +1,3 @@
+insert into greetings (language, greeting) values('zulu', 'sawubona');
+insert into greetings (language, greeting) values('xhosa', 'molo');
+insert into greetings (language, greeting) values('tsonga', 'abusheni');
